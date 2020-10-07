9cd1f277a7353b1b90077528a7b55c4d18aa078c
Marcus Vine <marcus.vine@uoit.net>
2020-10-06 22:44:28 -0400

    Added Basic Character Controller and Test Scene

---
+++ Engines_Unity_Portion/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 m_MovementDir;
+    private bool m_Grounded = true;
+    private float m_X;
+    private float m_Z;
+    private Vector3 m_Velocity;
+
+    public CharacterController m_Controller;
+    public Transform m_GroundCheck;
+    public LayerMask m_GroundMask;
+
+    public float m_GroundCheckRadius = 0.5f;
+    public float m_MoveSpeed = 10.0f;
+    public float m_Gravity = -9.81f;
+    public float m_JumpHeight = 2.0f;
+
+// Update is called once per frame
+    private void Update()
+    {
+        m_Grounded = Physics.CheckSphere(m_GroundCheck.position, m_GroundCheckRadius, m_GroundMask);
+
+
+        if (m_Grounded && m_Velocity.y < 0) m_Velocity.y = -2.0f;
+
+        m_X = Input.GetAxis("Horizontal"); //Horizontal returns A value between -1, 1 not really needed here but a nice feature
+        m_Z = Input.GetAxis("Vertical"); //Vetical returns A value between -1, 1
+
+        m_MovementDir = transform.right * m_X + transform.forward * m_Z;
+
+        m_Controller.Move(m_MovementDir * m_MoveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) m_Velocity.y = Mathf.Sqrt(m_JumpHeight * -2.0f * m_Gravity);
+
+        m_Velocity.y += m_Gravity * Time.deltaTime;
+
+        m_Controller.Move(m_Velocity * Time.deltaTime);
+    }
+}
---
+++ Engines_Unity_Portion/Assets/Scripts/FpsCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FpsCamera : MonoBehaviour
+{
+    private const float m_MouseSensConversion = 500.0f;
+    private float m_MouseX;
+    private float m_MouseY;
+
+    public float m_MouseSensitivity;
+    public Transform m_PlayerBody;
+
+    private float m_XRotation = 0.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        m_MouseSensitivity += m_MouseSensConversion;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_MouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.deltaTime;
+        m_MouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.deltaTime;
+
+        m_XRotation -= m_MouseY;
+        m_XRotation = Mathf.Clamp(m_XRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(m_XRotation, 0.0f, 0.0f);
+        m_PlayerBody.Rotate(Vector3.up * m_MouseX);
+    }
+}
