608f85b8c4c3613493435f6e675448d6683499e6
Marcus Vine <marcus.vine@uoit.net>
2020-10-07 11:50:08 -0400

    Added Sprinting

--- Engines_Unity_Portion/Assets/Scripts/CharacterMotor.cs
+++ Engines_Unity_Portion/Assets/Scripts/CharacterMotor.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.WebCam;
 
 public class CharacterMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Private Variables
     private Vector3 m_MovementDir;
-    private bool m_Grounded = true;
+    private Vector3 m_Velocity;
     private float m_X;
     private float m_Z;
-    private Vector3 m_Velocity;
+    private bool m_Grounded = false;
+    private bool m_Sprinting = false;
 
+    //Public References
     public CharacterController m_Controller;
     public Transform m_GroundCheck;
     public LayerMask m_GroundMask;
+    public LayerMask m_DeathMask;
 
+    //Public Variables
+    public float m_SprintMultiplier = 10.0f;
     public float m_GroundCheckRadius = 0.5f;
     public float m_MoveSpeed = 10.0f;
     public float m_Gravity = -9.81f;
     public float m_JumpHeight = 2.0f;
 
 // Update is called once per frame
+
+
     private void Update()
     {
         m_Grounded = Physics.CheckSphere(m_GroundCheck.position, m_GroundCheckRadius, m_GroundMask);
@@ -31,9 +39,27 @@ public class CharacterMotor : MonoBehaviour
         m_X = Input.GetAxis("Horizontal"); //Horizontal returns A value between -1, 1 not really needed here but a nice feature
         m_Z = Input.GetAxis("Vertical"); //Vetical returns A value between -1, 1
 
+
         m_MovementDir = transform.right * m_X + transform.forward * m_Z;
 
-        m_Controller.Move(m_MovementDir * m_MoveSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            m_Sprinting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            m_Sprinting = false;
+        }
+
+        if (m_Sprinting)
+        {
+            m_Controller.Move(m_MovementDir * m_MoveSpeed * m_SprintMultiplier * Time.deltaTime);
+        }
+        else
+        {
+            m_Controller.Move(m_MovementDir * m_MoveSpeed * Time.deltaTime);
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space)) m_Velocity.y = Mathf.Sqrt(m_JumpHeight * -2.0f * m_Gravity);
 
@@ -41,4 +67,14 @@ public class CharacterMotor : MonoBehaviour
 
         m_Controller.Move(m_Velocity * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider p_collider)
+    {
+        if (p_collider.tag == "Death")
+        {
+            Debug.Log("Hey you died");
+        }
+    }
+
+
 }
